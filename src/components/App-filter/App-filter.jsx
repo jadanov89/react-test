ce800d766e087a968d0504fe1f18f82844213684
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2023-06-30 21:20:33 +0300

    3

---
+++ src/components/App-filter/App-filter.jsx
@@ -0,0 +1,20 @@
+const AppFilter = () => {
+    return (
+        <div className="btn-gruup">
+            <button type="button"
+                    className="btn btn-light">
+                    Все сотрудники
+            </button>
+            <button type="button"
+                    className="btn btn-outline-light">
+                    На повышение
+            </button>
+            <button type="button"
+                    className="btn btn-outline-light">
+                    З/П больше 1000$
+            </button>
+        </div>
+    )
+}
+
+export default AppFilter;
